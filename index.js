c217286ae91b80cec55bf918159f5c617c7eae74
unknown <master@NSN119136.edu119.local>
2023-12-15 10:08:04 -0300

    Arquivos do ProjetoZ

---
+++ index.js
@@ -0,0 +1,16 @@
+const render = require("ejs")
+const express = require("express")
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.get("/perguntar",(req,res)=>{
+    res.render("perguntar")
+})
+
+app.listen(8081,()=>{console.log("Servidor Iniciado com Sucasso!")})
